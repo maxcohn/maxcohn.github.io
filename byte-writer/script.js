cb78fd6283ee4ecbb5fda025aff69e03f5f20840
Maxwell Cohn <maxc1234@hotmail.com>
2015-07-30 13:03:32 -0400

    updated byte writer to version 3

--- byte-writer/script.js
+++ byte-writer/script.js
@@ -87,41 +87,95 @@ function buyBmp(){
 
 //buy gif (5/s)
 function buyGif(){
-	var gifCost = Math.floor(150 * Math.pow(1.1,gif));
+	var gifCost = Math.floor(250 * Math.pow(1.1,gif));
     if(bytes >= gifCost){
         gif = gif + 1;
     	bytes = bytes - gifCost;
         document.getElementById('gif').innerHTML = gif;
         document.getElementById('bytes').innerHTML = bytes;
     };
-    var nextCost = Math.floor(100 * Math.pow(1.1,gif));
+    var nextCost = Math.floor(250 * Math.pow(1.1,gif));
     document.getElementById('gifCost').innerHTML = nextCost;	
 }
 
 //buy mp3 (7/s)
 function buyMp3(){
-	var mp3Cost = Math.floor(150 * Math.pow(1.1,mp3));
+	var mp3Cost = Math.floor(400 * Math.pow(1.1,mp3));
     if(bytes >= mp3Cost){
         mp3 = mp3 + 1;
     	bytes = bytes - mp3Cost;
         document.getElementById('mp3').innerHTML = mp3;
         document.getElementById('bytes').innerHTML = bytes;
     };
-    var nextCost = Math.floor(250 * Math.pow(1.1,mp3));
+    var nextCost = Math.floor(400 * Math.pow(1.1,mp3));
     document.getElementById('mp3Cost').innerHTML = nextCost;	
 }
 
 //buy wav (7/s)
 function buyWav(){
-	var wavCost = Math.floor(150 * Math.pow(1.1,wav));
+	var wavCost = Math.floor(400 * Math.pow(1.1,wav));
     if(bytes >= wavCost){
         wav = wav + 1;
     	bytes = bytes - wavCost;
         document.getElementById('wav').innerHTML = wav;
         document.getElementById('bytes').innerHTML = bytes;
     };
-    var nextCost = Math.floor(250 * Math.pow(1.1,wav));
+    var nextCost = Math.floor(400 * Math.pow(1.1,wav));
     document.getElementById('wavCost').innerHTML = nextCost;	
+}
+
+//Starting at upgrade-area3, buy methods are now buy_"name of format" instead of buy"name of format, capitalized"
+
+//buy flac (9/s)
+function buy_flac(){
+	var flacCost = Math.floor(800 * Math.pow(1.1,flac));
+    if(bytes >= flacCost){
+        flac = flac + 1;
+    	bytes = bytes - flacCost;
+        document.getElementById('flac').innerHTML = flac;
+        document.getElementById('bytes').innerHTML = bytes;
+    };
+    var nextCost = Math.floor(800 * Math.pow(1.1,flac));
+    document.getElementById('flacCost').innerHTML = nextCost;
+}
+
+//buy mp4 (12/s)
+function buy_mp4(){
+	var mp4Cost = Math.floor(1000 * Math.pow(1.1,mp4));
+    if(bytes >= mp4Cost){
+        mp4 = mp4 + 1;
+    	bytes = bytes - mp4Cost;
+        document.getElementById('mp4').innerHTML = mp4;
+        document.getElementById('bytes').innerHTML = bytes;
+    };
+    var nextCost = Math.floor(1000 * Math.pow(1.1,mp4));
+    document.getElementById('mp4Cost').innerHTML = nextCost;
+}
+
+//buy wmv (12/s)
+function buy_wmv(){
+	var wmvCost = Math.floor(1000 * Math.pow(1.1,wmv));
+    if(bytes >= wmvCost){
+        wmv = wmv + 1;
+    	bytes = bytes - wmvCost;
+        document.getElementById('wmv').innerHTML = wmv;
+        document.getElementById('bytes').innerHTML = bytes;
+    };
+    var nextCost = Math.floor(1000 * Math.pow(1.1,wmv));
+    document.getElementById('wmvCost').innerHTML = nextCost;
+}
+
+//buy avi (12/s)
+function buy_avi(){
+	var aviCost = Math.floor(1000 * Math.pow(1.1,avi));
+    if(bytes >= aviCost){
+        avi = avi + 1;
+    	bytes = bytes - aviCost;
+        document.getElementById('avi').innerHTML = avi;
+        document.getElementById('bytes').innerHTML = bytes;
+    };
+    var nextCost = Math.floor(1000 * Math.pow(1.1,avi));
+    document.getElementById('aviCost').innerHTML = nextCost;
 }
 
 
@@ -136,5 +190,9 @@ window.setInterval(function(){
 	writeByte(gif * 5);
 	writeByte(mp3 * 7);
 	writeByte(wav * 7);
+	writeByte(flac * 9);
+	writeByte(mp4 * 12);
+	writeByte(wmv * 12);
+	writeByte(avi * 12);
 	
 }, 1000);
